28379754c8e76c9778936d45cc302b9860589c20
darshanime <deathbullet@gmail.com>
2018-03-07 19:21:50 +0530

    add dict meaning, did you mean

--- main.go
+++ main.go
@@ -37,13 +37,41 @@ func run(c *cli.Context) error {
 		log.Fatal(err)
 	}
 
+	res := Results{}
+
+	// Found Meanings
 	doc.Find("div.wordMeanings.didYouMean div.dict_match ul.clearfix li div.dict_card ").Each(
 		func(i int, s *goquery.Selection) {
 			word := s.Find("div.dict_card_left h4").Text()
-			meanings := s.Find("div.dict_card_right h4").Text()
+			meaning := fmt.Sprintf("%s %s", s.Find("div.dict_card_right h4").Eq(0).Text(), s.Find("div.dict_card_right h4").Eq(2).Text())
 			urdu := s.Find("div.dict_card_left p.meaningUrduText").Text()
-			fmt.Printf("%d: %s (%s) - %s\n", i+1, word, urdu, meanings)
+			res.Meanings = append(res.Meanings, MeaningPairs{Word: fmt.Sprintf("%s (%s)", word, urdu), Meaning: meaning})
 		})
+
+	// Dictionary meaning
+	heading := doc.Find("div.meaningDetailContainer ul li div div span div2 span").Text()
+	description := doc.Find("div.meaningDetailContainer ul li div div span div2 p").Text()
+
+	res.Dictionary = DictionaryMeaning{Word: heading, Meaning: description}
+
+	// Did you mean
+	doc.Find("div.search_word ul li").Each(
+		func(i int, s *goquery.Selection) {
+			did_you_mean := s.Find("a").Text()
+			res.Word_suggestions = append(res.Word_suggestions, did_you_mean)
+		})
+
+	results_tmpl, err := template.New("Meanings").Parse(results_template)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = results_tmpl.Execute(os.Stdout, res)
+
+	if err != nil {
+		panic(err)
+	}
 
 	return nil
 }
