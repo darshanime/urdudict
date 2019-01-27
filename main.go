4719167da504817f8060925d80c0e07b1fddaf1f
darshanime <deathbullet@gmail.com>
2019-01-27 14:03:55 +0530

    update for changes to rekhta dom

--- main.go
+++ main.go
@@ -12,28 +12,20 @@ import (
 )
 
 const (
-	app_name         string = "urdudict"
-	app_usage        string = "Urdu dict in your terminal"
-	app_version             = "0.2.0"
-	rekhta                  = "https://www.rekhta.org/urdudictionary/?lang=1&keyword="
-	results_template string = `{{ if .Meanings }}
-Found meanings:
-~~~~~~~~~~~~~~~
-{{range $key, $value := .Meanings }}
-{{ $value.Word }} - {{ $value.Meaning }} {{end}}
-{{ if .Dictionary.Word }}
-Dictionary meanings:
-~~~~~~~~~~~~~~~~~~~~
-{{ .Dictionary.Word }}
-{{ .Dictionary.Meaning }}
-{{ end }}{{ if .Word_suggestions }}
+	appName         string = "urdudict"
+	appUsage        string = "Urdu dict in your terminal"
+	appVersion             = "0.3.0"
+	rekhta                 = "https://www.rekhta.org/urdudictionary/?lang=1&keyword="
+	resultsTemplate string = `{{ if .Meanings }}Found meaning
+~~~~~~~~~~~~~
+{{range $key, $value := .Meanings }}{{ $value.Word }} - {{ $value.Meaning }} {{end}}
+{{ if .WordSuggestions }}
 Did you mean
 ~~~~~~~~~~~~
-{{range $value := .Word_suggestions }} {{ $value }} {{end}} {{ end }}
+{{range $value := .WordSuggestions }}{{ $value }} {{end}} {{ end }}
 
 Source: rekhta.org
-{{ else }}No meanings found{{ end }}
-`
+{{ else }}No meanings found{{ end }}`
 )
 
 type MeaningPairs struct {
@@ -41,15 +33,9 @@ type MeaningPairs struct {
 	Meaning string
 }
 
-type DictionaryMeaning struct {
-	Word    string
-	Meaning string
-}
-
 type Results struct {
-	Meanings         []MeaningPairs
-	Dictionary       DictionaryMeaning
-	Word_suggestions []string
+	Meanings        []MeaningPairs
+	WordSuggestions []string
 }
 
 type InvalidArgsError struct {
@@ -65,17 +51,9 @@ func run(c *cli.Context) error {
 		return &InvalidArgsError{c}
 	}
 
-	var show_dict bool
-	// true if asked for dict meaning
-	if c.BoolT("hide_dictionary") {
-		show_dict = false
-	} else {
-		show_dict = true
-	}
+	queryWord := c.Args().First()
 
-	query_word := c.Args().First()
-
-	doc, err := goquery.NewDocument(rekhta + query_word)
+	doc, err := goquery.NewDocument(rekhta + queryWord)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -83,37 +61,26 @@ func run(c *cli.Context) error {
 	res := Results{}
 
 	// Found Meanings
-	doc.Find("div.wordMeanings.didYouMean div.dict_match ul.clearfix li div.dict_card ").Each(
-		func(i int, s *goquery.Selection) {
-			word := s.Find("div.dict_card_left h4").Text()
-			meaning := fmt.Sprintf("%s %s", s.Find("div.dict_card_right h4").Eq(0).Text(), s.Find("div.dict_card_right h4").Eq(2).Text())
-			urdu := s.Find("div.dict_card_left p.meaningUrduText").Text()
-			res.Meanings = append(res.Meanings, MeaningPairs{Word: fmt.Sprintf("%s (%s)", word, urdu), Meaning: meaning})
-		})
-
-	// Dictionary meaning
-	if show_dict == true {
-		heading := doc.Find("div.meaningDetailContainer ul li div div span div2 span").Text()
-		description := doc.Find("div.meaningDetailContainer ul li div div span div2 p").Text()
-		res.Dictionary = DictionaryMeaning{Word: heading, Meaning: description}
-	} else {
-		res.Dictionary = DictionaryMeaning{}
-	}
+	// only single word meaning in v0.3
+	res.Meanings = []MeaningPairs{
+		MeaningPairs{
+			Word:    fmt.Sprintf("%s (%s)", strings.TrimSpace(doc.Find(".dicSrchWord").Text()), doc.Find(".dicSrchMnngUrdu").Text()),
+			Meaning: doc.Find(".dicSrchWrdSyno").Text()}}
 
 	// Did you mean
-	doc.Find("div.search_word ul li").Each(
+	doc.Find("a.didUMeanWrd").Each(
 		func(i int, s *goquery.Selection) {
-			did_you_mean := s.Find("a").Text()
-			res.Word_suggestions = append(res.Word_suggestions, did_you_mean)
+			didYouMean := s.Find("span").Text()
+			res.WordSuggestions = append(res.WordSuggestions, didYouMean)
 		})
 
-	results_tmpl, err := template.New("Meanings").Parse(results_template)
+	resultsTmpl, err := template.New("Meanings").Parse(resultsTemplate)
 
 	if err != nil {
 		panic(err)
 	}
 
-	err = results_tmpl.Execute(os.Stdout, res)
+	err = resultsTmpl.Execute(os.Stdout, res)
 
 	if err != nil {
 		panic(err)
@@ -124,17 +91,10 @@ func run(c *cli.Context) error {
 
 func main() {
 	app := cli.NewApp()
-	app.Name = app_name
-	app.Usage = app_usage
-	app.Version = app_version
+	app.Name = appName
+	app.Usage = appUsage
+	app.Version = appVersion
 	app.Action = run
-	app.Flags = []cli.Flag{
-		cli.BoolTFlag{
-			Name:  "hide_dictionary, hd",
-			Usage: "set to display the dictionary meaning",
-		},
-	}
-
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
